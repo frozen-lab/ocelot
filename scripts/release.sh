a259e554f6366590cfe286f861aa0330c23fcbbf
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2025-11-02 00:24:47 +0530

    v0.0.1 init

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# create dir just in case
-mkdir -p .local
-
-awk 'BEGIN {print "## CHANGELOG"} 
-     /^### V-/ {if (found) exit; found=1; next} 
-     found && NF {print}' CHANGELOG.md > .local/release.md
